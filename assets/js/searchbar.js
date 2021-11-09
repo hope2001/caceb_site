d7abbc8611fb1f2b7ffc4e8da3be58fb747a8690
hope2001 <orounlaesp@gmail.com>
2021-11-09 23:59:19 +0100

    b

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -1,12 +1,14 @@
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+//const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const endpoint = 'assets/js/dataset.json';
 
 const cities = [];
 
 // fetch grabs endpoint - at this point a promise and generates readablestream
 fetch(endpoint)
+    
   .then(blob => blob.json())
   .then(data => cities.push(...data));
-
+    console.log(endpoint)
 function findMatches(keyword, cities) {
   return cities.filter(place => {
     // does city or state match? use paramater regex
