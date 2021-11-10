5e39784d6b3fa45143a79d1c3b13a90800d5d075
hope2001 <orounlaesp@gmail.com>
2021-11-10 15:26:50 +0100

    --- Auto-Git Commit ---

--- assets/js/searchbar.js
+++ assets/js/searchbar.js
@@ -26,45 +26,47 @@ function displayMatches() {
        html = ''
    }
    else
-   {}
+   {
     html = matchArray.map(place => {
     
-    const regex = new RegExp(this.value, 'gi');
-    const socity_name = place.socity.replace(regex, `<span class="highlight">${this.value}</span>`);
-    const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-    
-    return `
-      <div class="col-lg-6">
-      <div
-        class="member d-flex align-items-start"
-        data-aos="zoom-in"
-        data-aos-delay="100"
-      >
-        <div class="pic">
-          <img
-            src="${place.logo}"
-            class="img-fluid"
-            alt=""
-          />
-        </div>
-        <div class="member-info">
-          <h4>${place.socity}</h4>
-          <span>${place.identifier}</span>
-          <p>
-          ${place.Description}
-          </p>
-          <div class="social">
-            <a href="${place.website}"><i class="ri-google-fill"></i></a>
-            <a href=""><i class="ri-twitter-fill"></i></a>
-            <a href=""><i class="ri-facebook-fill"></i></a>
-            <a href=""><i class="ri-instagram-fill"></i></a>
-            <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+        const regex = new RegExp(this.value, 'gi');
+        const socity_name = place.socity.replace(regex, `<span class="highlight">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        
+        return `
+          <div class="col-lg-6">
+          <div
+            class="member d-flex align-items-start"
+            data-aos="zoom-in"
+            data-aos-delay="100"
+          >
+            <div class="pic">
+              <img
+                src="${place.logo}"
+                class="img-fluid"
+                alt=""
+              />
+            </div>
+            <div class="member-info">
+              <h4>${place.socity}</h4>
+              <span>${place.identifier}</span>
+              <p>
+              ${place.Description}
+              </p>
+              <div class="social">
+                <a href="${place.website}"><i class="ri-google-fill"></i></a>
+                <a href=""><i class="ri-twitter-fill"></i></a>
+                <a href=""><i class="ri-facebook-fill"></i></a>
+                <a href=""><i class="ri-instagram-fill"></i></a>
+                <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    `;
-  }).join('');
+        `;
+      }).join('');
+   }
+
   
   if (html !== ''|| html == !null) {
     suggestions.innerHTML = html;
