517cec185a71d4dcee90513b5cf04d152b081b18
hope2001Lab <eorounla@bourjon.com>
2023-04-13 14:59:40 +0100

    add ESE2i

---
+++ assets/js/dataSet.js
@@ -0,0 +1,290 @@
+const data = [
+    {
+        "socity": "SIGMA 2-SA",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "STPCI Sarl",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "COLAS",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CERAB TP",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "GRACE DIVINE BTP",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "GROUPE GEANT",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "Est accréditée",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "accr": [
+            " - la section Assurance Qualité selon selon la norme <strong>NF EN ISO 9001/CEI 17025 sous le Numéro :1-9713</strong>      <br><br>- la section Gestion Environnementale selon selon la <strong>norme NF EN ISO 14001 sous le Numéro :1-7215</strong>      <br><br> - la section Hygiène, Santé et Sécurité selon selon la<strong> norme NF EN ISO 14001 sous le Numéro :1-0769</strong>      <br><br>"
+        ],
+        "website":"#"
+    },
+    {
+        "socity": "FATHNEL BTP",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "Est accréditée",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "accr": [
+            " - la section Assurance Qualité selon selon la norme <strong>NF EN ISO 9001/CEI 17025 sous le Numéro : 1-8954.</strong> <br><br> - la section Gestion Environnementale selon selon la norme<strong> NF EN ISO 14001 sous le Numéro : 1-6874</strong><br><br> -la section Hygiène, Santé et Sécurité selon selon la <strong>norme NF EN ISO 14001 sous le Numéro : 1-0687</strong> <br><br>"
+        ],
+        "website":"#"
+    },
+    
+    {
+        "socity": "EE BTP / EBR Sarl",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "TEB",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CERAB",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "LOGIC",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "ENERGIE DIEU-DONNE",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "LA LICORNE",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "ZENITH SERVICE",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "MAYARICK",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "MMC-TOGO",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "TOGO",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CHAABANE",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CGE",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "OUMAROU KANAZOE",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CRN 5IG",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CCRSGC",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "Bénin",
+        "Description": "Accrédité par le CACeB",
+        "website":"#"
+    },
+    {
+        "socity": "CHINOHYPROCORPO",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "CHINE",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "CGGC",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "8405837",  
+        "state": "CHINE",
+        "Description": "Activités hydrauliques",
+        "website":"#"
+    },
+    {
+        "socity": "BEST BUILDING SARL",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "Est accréditée",  
+        "state": "Bénin",
+        "Description": "Gestion Environnementale ",
+        "accr": [
+            "(Batiments, Voiries et Réseau Divers, Assainissement, Eau Potable, Energie, Environement )<br><br>- Dans la section Gestion Environementale  selon la norme <strong>NF EN ISO 14001 :2014 sous le numero 1-10984 </strong>       <br><br>"
+        ],
+        "website":"#"
+    },
+    {
+        "socity": "ESE2i SARL",
+        "logo": "assets/img/faqs.jpg",
+        "identifier": "Est accréditée",  
+        "state": "Bénin",
+        "Description": "Dans les Activités : Énergies - Réseaux Télécom et Électrique : ",
+        "accr": [
+            "- la section Assurance Qualité selon la norme <br><br> <strong>NF EN ISO 9001/CEI 17025 sous le Numéro : 1-8955 </strong><br><br>  - la section Gestion Environnementale selon selon la norme  <br><br> <strong> NF EN ISO 14001 isous le Numéro : 1-6875 </strong><br><br>  - la section Hygiène, Santé et Sécurité selon selon la norme <br><br> <strong> NF EN ISO 45001 isous le Numéro : 1-0689 </strong><br><br>"        ],
+        "website":"#"
+    }
+    
+]
+
+let dataToDisplay =[]
+
+// Get a reference to the search bar input element
+const searchBar = document.querySelector('#barRecherche');
+
+// Add an event listener to the search bar
+setTimeout(()=>{
+    // let void =
+    
+
+searchBar.addEventListener('input', () => {
+  // Get the search query from the input field
+  const searchQuery = searchBar.value.toLowerCase();
+
+// if(searchQuery !== ''){  // Filter the data based on the search query
+  const searchResults = [];
+
+  for (const field of data) {
+    //   if (item[field].socity.toLowerCase() == searchQuery) {
+      if ((JSON.stringify(field).toLowerCase()).indexOf(searchQuery) >=0) {
+        searchResults.push(field)
+      }
+    }
+
+    dataToDisplay = searchResults;
+
+  // Display the search results
+
+  const resultsList = document.querySelector('#SearchResult');
+  resultsList.innerHTML = '';
+  dataToDisplay.forEach(result => {
+    const li = document.createElement('div');
+    li.className = "col-lg-6"
+    li.innerHTML =`<div
+    class="member d-flex align-items-start"
+    data-aos="zoom-in"
+    data-aos-delay="100"
+  >
+    <div class="pic">
+      <img
+        src="${result.logo}"
+        class="img-fluid"
+        alt=""
+      />
+    </div>
+    <div class="member-info">
+      <h4>${result.socity}</h4>
+      <span>${result.identifier}</span>
+      <p>
+      ${result.Description && result.Description}
+      </p>
+      <p class="text-primary">
+        ${result.accr && result.accr}
+      
+      
+      </p>
+      
+      <div class="social">
+        <a href="${result.website}"><i class="ri-google-fill"></i></a>
+        <a href=""><i class="ri-twitter-fill"></i></a>
+        <a href=""><i class="ri-facebook-fill"></i></a>
+        <a href=""><i class="ri-instagram-fill"></i></a>
+        <a href=""> <i class="ri-linkedin-box-fill"></i> </a>
+      </div>
+    </div>
+  </div>`
+    resultsList.appendChild(li);
+  });
+//   }else{
+     
+    
+//     document.querySelector('#SearchResult').innerHTML = "Aucune donnée correspondante")
+//   }
+});}, 1000);
